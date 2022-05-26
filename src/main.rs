9e6ff8b433e640a5ecaedb5fb598ea158c63bcee
kodylow <kodylow7@gmail.com>
2022-05-25 18:16:33 -0700

    set up actix boilerplate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", &name)
+}
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    // backbone of application, sets up http server in single line
+    // handles all TRANSPORT level concerns
+    HttpServer::new(|| {
+        // where all the application logic lives: routing, middlewares, request handlers, etc
+        App::new()
+            .route("/", web::get().to(greet))
+            .route("/{name}", web::get().to(greet))
+    })
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
 }
