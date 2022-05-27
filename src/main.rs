b889fe85eab27b13331113dc61d334cf1ca037d8
kodylow <kodylow7@gmail.com>
2022-05-27 06:04:53 -0700

    first route handler

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+
 async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
     format!("Hello {}!", &name)
 }
+
+// health_check is a 200 status check handler for development
+async fn health_check(req: HttpRequest) -> impl Responder {
+    HttpResponse::Ok()
+}
+// tokio::main writes boilerplate to run async main as sync on top of tokio's runtime
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     // backbone of application, sets up http server in single line
